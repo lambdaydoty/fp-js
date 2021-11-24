6fe3603fdc58869912b41333ec9dae2e930ea87f
lambdaydoty <euphrates.tigris@gmail.com>
2021-11-24 12:59:30 +0000

    Clean up

--- purescript/02-getting-started/src/Main.purs
+++ purescript/02-getting-started/src/Main.purs
@@ -5,8 +5,11 @@ import Prelude
 import Effect (Effect)
 import Effect.Console (logShow)
 import Math (sqrt)
+import Data.List
 
 diagonal w h = sqrt (w * w + h * h)
 
 main :: Effect Unit
-main = logShow (diagonal 3.0 4.0)
+main = do
+  logShow (diagonal 3.0 4.0)
+  logShow (range 0 999)
